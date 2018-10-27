c8f4849f232da283eddff00b6dbcf6f211b110d0
relandoth <37215463+relandoth@users.noreply.github.com>
2018-10-27 22:24:45 +0800

    Update pemanggil.js

--- pemanggil.js
+++ pemanggil.js
@@ -25,7 +25,7 @@ if(x=="1"){xxx="/2018/10/how-to-predict-bitcoin-price-increase.html"}
 if(x=="2"){xxx="/2018/10/history-of-cryptocurrency.html"}
 if(x=="3"){xxx="/2018/10/simple-ways-to-get-free-bitcoin.html"}
 if(x=="4"){xxx="/2018/10/how-to-mining-bitcoin.html"}
-if(x=="5"){xxx="/2018/10/what-is-bitcoin.html"}a_to_ve[i].href="https://www.skip-ads.ml/"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
+if(x=="5"){xxx="/2018/10/what-is-bitcoin.html"}a_to_ve[i].href="https://www.skip-ads.ml"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
 var a_to_vj=document.getElementById("anonyminized");var a_to_vk=document.getElementById("found_links");if(a_to_vj)
 {a_to_vj.innerHTML+=a_to_vb;}
 if(a_to_vk)
